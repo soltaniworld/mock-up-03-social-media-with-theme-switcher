2edcb4639cdba5b321896fd9c5235e80c1434d3c
Arash Soltani <soltaniworld@gmail.com>
2020-09-13 13:15:59 -0400

    added variable.sass

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const toggle = document.querySelector("#toggle");
+const elementsLight = document.querySelectorAll("[class$='--light']"); //Will select all elements whos class ends with '--light'
+
+//Add click event for toggle switch
+toggle.addEventListener('click', ()=>{
+  //go through each element with --light class
+  //toggle --dark counterpart to each of those class
+  elementsLight.forEach((em)=>{
+    toggleDark(em, "--light", "--dark")
+  });
+});
+
+
+//toggle its counterpart [class]--dark forEach class that ends with [class]--light
+// end = "--light"
+// end-mod = '--dark" - the modified class to toggle
+function toggleDark (em, end, endMod){
+  em.classList.forEach((c)=>{
+    if (c.endsWith(end)){
+      em.classList.toggle(c.slice(0,-end.length)+endMod)
+    }
+  });
+}
